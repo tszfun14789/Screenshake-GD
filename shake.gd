42271b53d2f003b0e9f5a70d8cf27d2217782a2c
tszfun14789 <161189032+tszfun14789@users.noreply.github.com>
2024-03-11 14:45:55 +0800

    Update shake.gd

--- shake.gd
+++ shake.gd
@@ -10,12 +10,7 @@ var noise : FastNoiseLite
 
 func _ready():
 	# Generate noise for noise shake
-	#
-	# This is only generated once when the game starts
-	# and then read over and over again
-	#
-	# These parameters change the shape of the noise
-	# and the feel of the shake6
+
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	noise = FastNoiseLite.new()
 	noise.seed = randi()
@@ -25,11 +20,6 @@ func _ready():
 	
 
 func shake(intensity, duration, type = Type.Sine):
-	# Set the shake parameters
-	#
-	# A good idea here is to add configuration settings that
-	# allow the player to turn off shake
-	#
 	# if player_no_want:
 	# 	intensity = 0
 	
@@ -41,56 +31,30 @@ func shake(intensity, duration, type = Type.Sine):
 
 func _process(delta):
 	# Get the camera
-	#
-	# You'll need to adjust this depending on how you want to
-	# keep track of the active camera in your game
-	#
-	# Maybe your game has two cameras, maybe it has 10, who knows?
-	# Do what you like
 	if get_tree().current_scene.scene_file_path != "res://mainmenu.tscn" && get_tree().current_scene.scene_file_path != "res://intro.tscn" && get_tree().current_scene.filename != "res://settingmenu.tscn":
 		var camera = get_tree().current_scene.get_node("Player/Camera2D")	
-		# Stop shaking if the camera_shake_duration timer is down to zero
 		if camera_shake_duration <= 0:
-			# Reset the camera when the shaking is done
+
 			camera.offset = Vector2.ZERO
 			camera_shake_intensity = 0.0
 			camera_shake_duration = 0.0
 			return
-			
-		# Subtract the elapsed time from the camera_shake_duration
-		# so that it eventually ends
-		#
-		# You can do other fun stuff here too like have the intensity
-		# decay gradually so that the shake tapers off
+
 		camera_shake_duration = camera_shake_duration - delta
 			
-		# Shake it
+
 		var offset = Vector2.ZERO
 			
 		if camera_shake_type == Type.Random:
 			# Random shake
-			# Chaos
-			# Madness
-			#
-			# Personally, I like this best because players don't notice
-			# any difference in the thick of battle when the shakes are short
-			# and because it's dead simple.
 			offset = Vector2(randf(), randf()) * camera_shake_intensity
 
 		if camera_shake_type == Type.Sine:
 			# Sine wave based shake
-			#
-			# Play around with the magic numbers to adjust the feel
-			#
-			# Basing the sine wave off of get_ticks_msec ensures that
-			# the returned value is continuous and smooth
 			offset = Vector2(sin(Time.get_ticks_msec() * 0.03), sin(Time.get_ticks_msec() * 0.07)) * camera_shake_intensity * 0.5
 			
 		if camera_shake_type == Type.Noise:
 			# Noise based shake
-			#
-			# Accessing the noise based on get_ticks_msec ensures that
-			# the returned value is continuous and smooth
 			var noise_value_x = noise.get_noise_1d(Time.get_ticks_msec() * 0.1)
 			var noise_value_y = noise.get_noise_1d(Time.get_ticks_msec() * 0.1 + 100.0)
 			offset = Vector2(noise_value_x, noise_value_y) * camera_shake_intensity * 2.0
